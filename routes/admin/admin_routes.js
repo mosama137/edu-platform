4426a68221679f681a558a5c5500b17448f3a8d4
mosama137 <m.osama202015@gmail.com>
2024-04-30 10:59:03 +0300

    updating routes

--- routes/admin/admin_routes.js
+++ routes/admin/admin_routes.js
@@ -5,16 +5,20 @@ const adminController = require('../../controllers/admin.controller')
 // route matches api/v1/admin/
 
 // fetching data
-adminRouter.get('/subjects', adminController.getSubjects)
+adminRouter.get('/get-subjects', adminController.getSubjects)
 adminRouter.get('/get-students', adminController.getStudents)
 adminRouter.get('/get-teachers', adminController.getTeachers)
 
-
-
-// control 
 adminRouter.post('/account-activate', adminController.activeAccount)
+
+
+// control subjects
 adminRouter.post('/add-subject', adminController.addSubject)
 adminRouter.post('/update-subject-teacher', adminController.updateSubjectTeacher)
+
+
+// deleting 
+adminRouter.delete('/del-user', adminController.delUser)
 adminRouter.delete('/del-subject', adminController.delSubject)
 
 
