9ad8e6d216a4dd9c1ec49587b73a03aeb177a252
mosama137 <m.osama202015@gmail.com>
2024-05-02 00:06:42 +0300

    add exam schema

---
+++ models/exam/exam.model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const questionSchema = new Schema({
+    questionText: String,
+    options: [String],
+    correctOptionIndex: Number
+})
+
+
+
+const examSchema = new Schema({
+    subject_id: {
+        type: Schema.ObjectId,
+        ref: "Subject"
+    },
+    title: String,
+    startAt: Date,
+    duration: Number,
+    questions: [questionSchema]
+});
+
+
+const Exam = mongoose.model('Exam', examSchema);
+
+module.exports = Exam;
--- models/index.js
+++ models/index.js
@@ -3,6 +3,7 @@ const Student = require('./basedOnRole/student.model')
 const Teacher = require('./basedOnRole/teacher.model')
 const Admin = require('./basedOnRole/admin.model')
 const Subject = require('./subject/subject_model')
+const Exam = require('./exam/exam.model')
 
 module.exports = {
     User,
@@ -10,4 +11,5 @@ module.exports = {
     Teacher,
     Admin,
     Subject,
+    Exam,
 }
