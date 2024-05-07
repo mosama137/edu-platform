cab1c3bb9693f7b934e234fa80b608c6f1893662
mosama137 <m.osama202015@gmail.com>
2024-05-08 00:50:11 +0300

    optimizing auth and admin controller

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -3,6 +3,33 @@ const createError = require('http-errors')
 
 
 // getting data
+// matches /api/v1/admin/get-subjects
+const getSubjects = async (req, res, next) => {
+    try {
+        const subjects = await Subject.find({}).populate({
+            path: 'teacher_id',
+            select: '_id',
+            populate: {
+                path: '_id',
+                select: "full_name"
+            } // Include only the 'name' field from the Teacher model
+        }).select('subject_name level teacher_id')
+
+        const formattedSubjects = subjects.map(subject => ({
+            subject_id: subject._id,
+            subject_name: subject.subject_name,
+            level: subject.level,
+            // Check if teacher_id and its nested _id exist before accessing full_name
+            teacher_name: subject.teacher_id && subject.teacher_id._id ? subject.teacher_id._id.full_name : null,
+            teacher_id: subject.teacher_id ? subject.teacher_id._id._id : null
+
+        }))
+        res.send(formattedSubjects)
+    } catch (error) {
+        next(createError.BadRequest())
+    }
+}
+// matches /api/v1/admin/get-students
 const getStudents = async (req, res, next) => {
     try {
         const students = await Student.find({}).populate({
@@ -13,7 +40,7 @@ const getStudents = async (req, res, next) => {
             .lean();
         // Remap the results to match your desired output structure
         const formattedStudents = students.map(student => ({
-            _id: student._id._id,
+            student_id: student._id._id,
             full_name: student._id.full_name,
             email: student._id.email,
             isActive: student._id.isActive,
@@ -24,6 +51,7 @@ const getStudents = async (req, res, next) => {
         next(createError.BadRequest())
     }
 }
+// matches /api/v1/admin/get-teachers
 const getTeachers = async (req, res, next) => {
     try {
         const teachers = await Teacher.find({}).populate({
@@ -44,33 +72,10 @@ const getTeachers = async (req, res, next) => {
         next(createError.BadRequest())
     }
 }
-const getSubjects = async (req, res, next) => {
-    try {
-        const subjects = await Subject.find({}).populate({
-            path: 'teacher_id',
-            select: '_id',
-            populate: {
-                path: '_id',
-                select: "full_name"
-            } // Include only the 'name' field from the Teacher model
-        }).select('subject_name level teacher_id')
 
-        const formattedSubjects = subjects.map(subject => ({
-            _id: subject._id,
-            subject_name: subject.subject_name,
-            level: subject.level,
-            // Check if teacher_id and its nested _id exist before accessing full_name
-            teacher_name: subject.teacher_id && subject.teacher_id._id ? subject.teacher_id._id.full_name : null,
-            teacher_id: subject.teacher_id ? subject.teacher_id._id._id : null
-
-        }))
-        res.send(formattedSubjects)
-    } catch (error) {
-        next(createError.BadRequest())
-    }
-}
 
 //  control accounts activation
+// matches /api/v1/admin/account-activate
 const activeAccount = async (req, res, next) => {
     try {
         const { user_id, active_value } = req.body
@@ -81,15 +86,15 @@ const activeAccount = async (req, res, next) => {
         },
             { new: true }
         )
-        res.send(user)
+        res.status(201).send({ msg: "done!" })
 
     } catch (error) {
         next(createError.BadRequest())
     }
 }
 
-
 // Subjects control
+// matches /api/v1/admin/add-subject
 const addSubject = async (req, res, next) => {
     try {
         const { teacher_id } = req.body //get teacher user_id
@@ -109,23 +114,25 @@ const addSubject = async (req, res, next) => {
     }
 }
 // update subject's teacher
+// matches /api/v1/admin/update-subject-teacher
 const updateSubjectTeacher = async (req, res, next) => {
     try {
         const { teacher_id, subject_id } = req.body
-        const subject = await Subject.findByIdAndUpdate(subject_id, {
-            $set: {
-                teacher_id: teacher_id
-            }
-        },
-            { new: true }
-        )
-        await Teacher.findByIdAndUpdate(teacher_id, {
+        const teacher = await Teacher.findByIdAndUpdate(teacher_id, {
             //we use add to set to compare and didn't duplicate 
             $addToSet: {
                 subjects: subject_id
             }
-        })
-        return res.send(subject)
+        }, { new: true })
+        if (teacher) {
+            await Subject.findByIdAndUpdate(subject_id, {
+                $set: {
+                    teacher_id: teacher_id
+                }
+            }
+            )
+        }
+        return res.send(teacher)
     } catch (error) {
         console.log(error);
         next(createError.BadRequest())
@@ -134,6 +141,7 @@ const updateSubjectTeacher = async (req, res, next) => {
 
 
 // deleting
+// matches /api/v1/admin/del-user
 const delUser = async (req, res, next) => {
     try {
         const { user_id } = req.body
@@ -151,6 +159,7 @@ const delUser = async (req, res, next) => {
         next(createError.BadRequest())
     }
 }
+// matches /api/v1/admin/del-subject
 const delSubject = async (req, res, next) => {
     try {
         const subject_id = req.body
--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,7 +1,6 @@
-const { User, Student, Teacher, Admin } = require('../models')
+const { User, Student, Teacher, Admin, Subject } = require('../models')
 const { signAccessToken } = require('../helpers/access_token')
 const createError = require('http-errors')
-
 
 const register = async function (req, res, next) {
     try {
@@ -16,14 +15,36 @@ const register = async function (req, res, next) {
             await Admin.create({ _id: user.id })
         }
         const accessToken = await signAccessToken(user)
-        //send token
+
+        const userFormattedData = {
+            user_id: user._id,
+            full_name: user.full_name,
+            national_id: user.national_id,
+            isActive: user.isActive,
+            email: user.email,
+            role: user.role
+        }
+
+        // If user is a student, fetch student level and subjects
+        if (user.role === "student") {
+            const student = await Student.findById(user._id);
+            if (student) {
+                userFormattedData.level = student.level;
+                // Fetch subjects based on student level
+                const subjects = await Subject.find({ level: student.level }).select("subject_name");
+                userFormattedData.subjects = subjects;
+            }
+        }
+
+        //send token and user Data
         return res.send(
-            { token: accessToken }
+            { token: accessToken, user: userFormattedData }
         )
 
     } catch (error) {
         // Handling Duplicate Key Error (adding id already exists)/ MongoServerError
         // user already exists!
+        console.log(error)
         if (error.code === 11000) {
             return next(createError.Conflict('Invalid National ID'))
         }
@@ -50,9 +71,40 @@ const login = async function (req, res, next) {
             if (!isPasswordValid) throw createError.Conflict("Wrong username or password")
             // get token
             const accessToken = await signAccessToken(user.id)
+
+            const userFormattedData = {
+                user_id: user._id,
+                full_name: user.full_name,
+                national_id: user.national_id,
+                isActive: user.isActive,
+                email: user.email,
+                role: user.role
+            }
+
+            // If user is a student, fetch student level and subjects
+            if (user.role === "student") {
+                const student = await Student.findById(user._id);
+                if (student) {
+                    userFormattedData.level = student.level;
+                    // Fetch subjects based on student level
+                    const subjects = await Subject.find({ level: student.level }).select("subject_name");
+                    userFormattedData.subjects = subjects;
+                }
+            }
+            // If user is a teacher, fetch assigned subjects
+            if (user.role === 'teacher') {
+                // Assuming teachers have an array of subject IDs they teach
+                const teacher = await Teacher.findById(user._id);
+                if (teacher) {
+                    // Fetch subjects based on assigned subject IDs
+                    const subjects = await Subject.find({ _id: { $in: teacher.subjects } }).select('subject_name _id level ');
+                    userFormattedData.subjects = subjects.map(subject => subject);
+                }
+            }
+
             //send token
             return res.send(
-                { token: accessToken }
+                { token: accessToken, user: userFormattedData }
             )
         }
 
